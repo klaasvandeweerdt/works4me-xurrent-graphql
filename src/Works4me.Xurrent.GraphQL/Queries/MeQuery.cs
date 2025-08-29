7437520e21139cbc0abed8e0da0ae6078173f54c
Klaas Vandeweerdt <klaas_vandeweerdt@hotmail.com>
2025-08-29 04:06:47 +0200

    Remove Search and CustomFilter methods from MeQuery

--- src/Works4me.Xurrent.GraphQL/Queries/MeQuery.cs
+++ src/Works4me.Xurrent.GraphQL/Queries/MeQuery.cs
@@ -235,51 +235,5 @@ namespace Works4me.Xurrent.GraphQL
 
             return Select("workHours", query, false);
         }
-
-        /// <summary>
-        /// <br>Filter the <see cref="Person">Person</see> by their custom fields that have been marked as "Filterable" in their UI Extension.</br>
-        /// <br>Additional information is available on the <see href="https://developer.xurrent.com/graphql/input_object/personcustomfilter/">Xurrent developer site</see>.</br>
-        /// </summary>
-        /// <param name="name">The name of the custom filter (i.e. value of the data-filterable-name attribute of the field in the UI Extension).</param>
-        /// <param name="op">The filter operator.</param>
-        /// <param name="values">
-        /// Use value <c>null</c> to indicate "None".
-        /// 
-        /// For "text" fields:
-        /// • One can start the value with <c>~</c> to get partial matches (start with <c>'~</c> to match on a value starting with <c>~</c>).
-        /// 
-        /// For "number" and "time" fields:
-        /// • One can start the value with <c>&lt;</c>, <c>&gt;</c>, <c>&lt;=</c>, <c>&gt;=</c> to get relative matches.
-        /// • Times should be formatted as HH:mm:ss (e.g. <c>16:00:00</c>). The seconds part, <c>:ss</c>, is optional so <c>16:00</c> is also valid.
-        /// • <c>&gt;{lower}&lt;{upper}</c> can be used to get matches where the value is more than '{lower}' and less than '{upper}'.
-        /// • <c>&gt;={lower}&lt;={upper}</c> can be used to get matches where the value is between '{lower}' and '{upper}'.
-        /// • <c>&gt;={lower}&lt;{upper}</c> can be used to get matches where the value is at least '{lower}' and less than '{upper}'.
-        /// 
-        /// For "date" and "date-time" fields:
-        /// • One must supply a single value (and <c>null</c> is not supported).
-        /// • Matches are always relative with an optional upper bound.
-        /// • Dates should be formatted as <c>yyyy-MM-dd</c> (e.g. <c>2020-05-20</c>).
-        /// • Date and time should be formatted as <c>yyyy-MM-ddTHH:mm:ssZ</c> (e.g. <c>2020-05-20T16:00:00Z</c>).
-        /// • <c>&gt;={lower}</c> should be used to get matches where the value is at least '{lower}'.
-        /// • <c>&gt;={lower}&lt;{upper}</c> can be used to get matches where the value is at least '{lower}' and before '{upper}'.
-        /// 
-        /// For "select" fields:
-        /// • One must supply labels to match in the language of the account (i.e. not the value attributes of the HTML options).
-        /// • Matches are case-sensitive.
-        /// </param>
-        public MeQuery CustomFilter(string name, FilterOperator op, params string?[] values)
-        {
-            return AddCustomFilter(name, op, values);
-        }
-
-        /// <summary>
-        /// Adds a free-form search filter to this <see cref="MeQuery"/>.
-        /// </summary>
-        /// <param name="value">The filter value.</param>
-        /// <returns>The same <see cref="MeQuery">MeQuery</see>, updated to include the "QueryFilter" sub-query.</returns>
-        public MeQuery Search(string value)
-        {
-            return AddQueryFilter(value);
-        }
     }
 }
